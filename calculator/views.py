f103ec76753bca8abd7761081aefe9d143656597
Leonid Taranov <supertaranov@gmail.com>
2023-03-07 22:37:57 +0700

    update

---
+++ calculator/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render
+
+DATA = {
+    'omlet': {
+        'яйца, шт': 2,
+        'молоко, л': 0.1,
+        'соль, ч.л.': 0.5,
+    },
+    'pasta': {
+        'макароны, г': 0.3,
+        'сыр, г': 0.05,
+    },
+    'buter': {
+        'хлеб, ломтик': 1,
+        'колбаса, ломтик': 1,
+        'сыр, ломтик': 1,
+        'помидор, ломтик': 1,
+    },
+    # можете добавить свои рецепты ;)
+}
+
+# Напишите ваш обработчик. Используйте DATA как источник данных
+# Результат - render(request, 'calculator/index.html', context)
+# В качестве контекста должен быть передан словарь с рецептом:
+# context = {
+#   'recipe': {
+#     'ингредиент1': количество1,
+#     'ингредиент2': количество2,
+#   }
+# }
+def home_view(request):
+    template_name = 'calculator/index.html'
+    return render(request, template_name)
+
+def recipes_view(request, recipe):
+    servings = int(request.GET.get("servings"))
+    context = {
+        'omlet': {
+            'яйца, шт': 2,
+            'молоко, л': 0.1,
+            'соль, ч.л.': 0.5,
+    },
+        'pasta': {
+            'макароны, г': 0.3,
+            'сыр, г': 0.05,
+    },
+        'buter': {
+            'хлеб, ломтик': 1,
+            'колбаса, ломтик': 1,
+            'сыр, ломтик': 1,
+            'помидор, ломтик': 1,       
+    }
+    }
+    return render(request, 'calculator/index.html', context)
