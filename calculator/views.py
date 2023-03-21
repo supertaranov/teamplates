7132b5d737c5efeddf915903d1470236bb379813
Leonid Taranov <supertaranov@gmail.com>
2023-03-21 17:50:07 +0700

    update

--- calculator/views.py
+++ calculator/views.py
@@ -33,22 +33,8 @@ def home_view(request):
     return render(request, template_name)
 
 def recipes_view(request, recipe):
-    servings = int(request.GET.get("servings"))
-    context = {
-        'omlet': {
-            'яйца, шт': 2,
-            'молоко, л': 0.1,
-            'соль, ч.л.': 0.5,
-    },
-        'pasta': {
-            'макароны, г': 0.3,
-            'сыр, г': 0.05,
-    },
-        'buter': {
-            'хлеб, ломтик': 1,
-            'колбаса, ломтик': 1,
-            'сыр, ломтик': 1,
-            'помидор, ломтик': 1,       
-    }
-    }
-    return render(request, 'calculator/index.html', context)
+    servings = int(request.GET.get("servings", 1))
+    recipe_dict = {}
+    for ingredient, amount in DATA[recipe].items():
+        recipe_dict[ingredient] = amount * servings
+    return render(request, 'calculator/index.html', {"recipe":recipe_dict})
